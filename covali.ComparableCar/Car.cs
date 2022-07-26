f2a25218f6613ef73d76d61105d43b87127309f8
Covali Andrei <krab4ek@gmail.com>
2022-07-26 22:30:30 +0300

    Add project files.

---
+++ covali.ComparableCar/Car.cs
@@ -0,0 +1,63 @@
+﻿namespace covali.ComparableCar;
+
+internal class Car:IComparable
+{
+    public const int MaxSpeed = 100;
+    public int CurrentSpeed { get; set; } = 0;
+    public string PetName { get; set; } = string.Empty;
+    public int CarID { get; set; }
+
+    // private bool carIsDead;
+
+    //private Radio theMusicBox = new Radio();
+
+    public Car()
+    {
+
+    }
+    public Car(string name, int speed, int carID)
+    {
+        CurrentSpeed = speed;
+        PetName = name;
+        CarID = carID;
+    }
+
+    public int CompareTo(object? obj)
+    {
+        Car? temp = obj as Car;
+        if (temp != null)
+        {
+            if (this.CurrentSpeed > temp.CurrentSpeed)
+                return 1;
+            if (this.CurrentSpeed < temp.CurrentSpeed)
+                return -1;
+            else
+                return 0;
+        }
+        else
+        {
+            throw new ArgumentException("Parameter is not a Car");
+        }
+    }
+
+    //public void CrankTunes(bool state)
+    //{
+    //    theMusicBox.TurnOn(state);
+    //}
+
+    //public void Accelerate(int delta)
+    //{
+    //    if (delta < 0)
+    //    {
+    //        throw new
+    //            ArgumentOutOfRangeException("delta","Speed must be  greater than zero!");
+    //    }
+    //    CarIsDeadException ex = new CarIsDeadException($"{PetName} has overheating",
+    //        "You have a lead foot",DateTime.Now);
+
+    //    ex.HelpLink = "http://www.test.com";
+    //    throw ex;
+
+
+    //}
+}
---
+++ covali.ComparableCar/Program.cs
@@ -0,0 +1,18 @@
+﻿using covali.ComparableCar;
+
+Console.WriteLine("***** Fun with Object Sorting*****\n");
+Console.WriteLine("Ilixa pokazhi YXO");
+Car[] myAuto = new Car[5];
+myAuto[0] = new Car("Rusty",55,1);
+myAuto[1] = new Car("Mary", 49, 234);
+myAuto[2] = new Car("Viper", 95, 34);
+myAuto[3] = new Car("Rusty", 40, 4);
+myAuto[4] = new Car("Chucky", 44, 5);
+Array.Sort(myAuto);
+foreach (Car c in myAuto)
+{
+    Console.WriteLine($"{c.CurrentSpeed}, {c.PetName}");
+}
+Console.ReadLine();
+
+
